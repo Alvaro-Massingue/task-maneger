d7da0c73b90e6c159e12367dd80095d41641ccf9
alvaromassingue <alvaroruimassingue25@gmail.com>
2026-01-18 21:45:25 +0200

    Adicionar id correcto para Google Analytics

--- src/analytics.js
+++ src/analytics.js
@@ -2,7 +2,7 @@ import ReactGA from "react-ga4";
 
 // Inicializa GA
 export const initGA = () => {
-  ReactGA.initialize("G-XXXXXXXXXX"); // substitua pelo seu ID
+  ReactGA.initialize("G-MN3L7KFQ7P");
 };
 
 // Envia pageview
